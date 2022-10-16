08c6749db97bd743607301284e9087274605bdad
Angela Lingan Tan <angelalingan@gmail.com>
2022-10-16 14:10:37 -0500

    added mongoose logic for all endpoints

--- index.js
+++ index.js
@@ -4,11 +4,23 @@ const express = require('express'),
   morgan = require('morgan');
   uuid = require('uuid');
 
-//Serving Static Files: instructing express to serve every file under the public directory
-app.use(express.static('public'));
+const mongoose = require('mongoose');
+const Models = require('./models.js');
+
+const Movies = Models.Movie;
+const Users = Models.User;
+const Genres = Models.Genre;
+const Directors = Models.Director;
+//const app = express();
+
+mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
 //Returns middleware that only parses JSON
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Serving Static Files: instructing express to serve every file under the public directory
+app.use(express.static('public'));
 
 //Logging with morgan
 app.use(morgan('common'));
@@ -38,7 +50,6 @@ let movies = [
       Name: 'James Cameron',
       Bio: 'A Canadian filmmaker. Best known for making science fiction and epic films',
       Birth: 'August 16, 1954',
-      Death: NULL
     },
   },
 
@@ -53,7 +64,6 @@ let movies = [
 			Name: 'James Cameron',
 			Bio: 'A Canadian filmmaker. Best known for making science fiction and epic films',
       Birth: 'August 16, 1954',
-      Death: NULL
 		},
 	},
 
@@ -68,7 +78,6 @@ let movies = [
 			Name: 'Matt Reeves',
 			Bio: 'An American film director, producer and screenwriter.',
       Birth: 'April 27, 1966',
-      Death: NULL
 		},
 	},
 
@@ -83,7 +92,6 @@ let movies = [
 			Name: 'Chris Columbus',
 			Bio: 'An American filmmaker. After writing screenplays for several teen comedies in the mid-1980s, he made his directorial debut with a teen adventure, Adventures in Babysitting (1987). Columbus gained recognition soon after with the highly successful Christmas comedy Home Alone (1990) and its sequel Home Alone 2: Lost in New York (1992).',
       Birth: 'September 10, 1958',
-      Death: NULL
 		},
 	},
 
@@ -98,7 +106,6 @@ let movies = [
 			Name: 'Jordan Peele',
 			Bio: 'An American actor, comedian, and filmmaker. He is best known for his film and television work in the comedy and horror genres.',
       Birth:'February 21, 1979',
-      Death: NULL
 		},
 	},
 
@@ -113,7 +120,6 @@ let movies = [
       Name: 'Jordan Peele',
       Bio: 'An American actor, comedian, and filmmaker. He is best known for his film and television work in the comedy and horror genres.',
       Birth:'February 21, 1979',
-      Death: NULL
     },
   },
 
@@ -128,7 +134,6 @@ let movies = [
 			Name: 'Barry Sonnenfeld',
 			Bio: 'An American filmmaker and television director.',
       Birth: 'April 1, 1953',
-      Death: NULL
 		},
 	},
 
@@ -143,7 +148,6 @@ let movies = [
 			Name: 'Steven Spielberg',
 			Bio: 'An American film director, producer and screenwriter.',
       Birth: 'December 18, 1946',
-      Death: NULL
 		},
 	},
 
@@ -158,7 +162,6 @@ let movies = [
 			Name: 'Andy Muschietti',
 			Bio: 'An Argentine filmmaker who achieved wide recognition with the 2013 film Mama which he made with Neil Cross and his sister, producer and screenwriter Barbara Muschietti, based on their three-minute film of the same name.',
       Birth:'August 26, 1973',
-      Death: NULL
 		},
 	},
 
@@ -173,126 +176,222 @@ let movies = [
       Name: 'Yeon Sang-ho',
       Bio: 'A South Korean film director and screenwriter. He gained international popularity for working his adult animated films The King of Pigs (2011) and The Fake (2013), and the live-action film Train to Busan (2016).',
       Birth:'December 25, 1978',
-      Death: NULL
     },
   },
 ];
 
-//CREATE: Allow new users to register
+// Allow new users to register
 app.post('/users', (req, res) => {
-  const newUser= req.body;
-
-  if (newUser.name) {
-    newUser.id = uuid.v4();
-    users.push(newUser);
-    res.status(201).json(newUser)
-  } else {
-    res.staus(400).send('Users need names')
-  }
+Users.findOne({ Username: req.body.Username })
+   .then((user) => {
+     if (user) {
+       return res.status(400).send(req.body.Username + 'already exists');
+     } else {
+       Users
+         .create({
+           Username: req.body.Username,
+           Password: req.body.Password,
+           Email: req.body.Email,
+           Birthday: req.body.Birthday
+         })
+         .then((user) =>{res.status(201).json(user) })
+       .catch((error) => {
+         console.error(error);
+         res.status(500).send('Error: ' + error);
+       })
+     }
+   })
+   .catch((error) => {
+     console.error(error);
+     res.status(500).send('Error: ' + error);
+   });
 });
 
-//UPDATE: Allow users to update their user info (username)
-app.put('/users/:id/', (req, res) => {
-  const { id } = req.params;
-  const updatedUser= req.body;
-
-  let user = users.find( user => user.id == id);
-
-  if (user) {
-    user.name =updatedUser.name;
-    res.status(200).json(user);
-  } else (
-    res.status(404).send('This user is not in the database')
-  )
+// Get all users
+app.get('/users', (req, res) => {
+  Users.find()
+  .then((users) => {
+    res.status(201).json(users);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  });
 });
 
-//CREATE: Allow users to add a movie to their list of favorites
-app.post('/users/:id/:movieTitle', (req, res) => {
-  const { id, movieTitle } = req.params;
+// Get a user by username
+app.get('/users/:Username', (req, res) => {
+  Users.findOne({ Username: req.params.Username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
 
-  let user = users.find( user => user.id == id);
+// Allow users to update their user info (username)
+app.put('/users/:Username', (req, res) => {
+  Users.findOneAndUpdate({ Username: req.params.Username }, { $set:
+    {
+      Username: req.body.Username,
+      Password: req.body.Password,
+      Email: req.body.Email,
+      Birthday: req.body.Birthday
+    }
+  },
+  { new: true }, // This line makes sure that the updated document is returned
+  (err, updatedUser) => {
+    if(err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    } else {
+      res.json(updatedUser);
+    }
+  });
+});
 
-  if (user) {
-    user.favoriteMovies.push(movieTitle);
-    res.status(200).send(`${movieTitle} has been added to user ${id}'s array`);
-  } else (
-    res.status(404).send('This user is not in the database')
-  )
+// Allow users to add a movie to their list of favorites
+app.post('/users/:Username/movies/:MovieID', (req, res) => {
+  Users.findOneAndUpdate({ Username: req.params.Username }, {
+     $push: { FavoriteMovies: req.params.MovieID }
+   },
+   { new: true }, // This line makes sure that the updated document is returned
+  (err, updatedUser) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    } else {
+      res.json(updatedUser);
+    }
+  });
 });
 
 //DELETE: Allow users to remove a movie from their list of favorites
 app.delete('/users/:id/:movieTitle', (req, res) => {
-  const { id, movieTitle } = req.params;
+  Users.findOneAndUpdate({ Username: req.params.Username}, {
+      $pull: { FavoriteMovies: req.params.MovieID }
+    },
+    { new: true },
+    (err, updatedUser) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      } else {
+        res.json(updatedUser);
+      }
+    });
+  });
+  //const { id, movieTitle } = req.params;
 
-  let user = users.find( user => user.id == id);
+  //let user = users.find( user => user.id == id);
 
-  if (user) {
-    user.favoriteMovies = user.favoriteMovies.filter( title => title !== movieTitle);
-    res.status(200).send(`${movieTitle} has been removed from user ${id}'s array`);
-  } else (
-    res.status(404).send('This user is not in the database')
-  )
-});
+  //if (user) {
+    //user.favoriteMovies = user.favoriteMovies.filter( title => title !== movieTitle);
+  //  res.status(200).send(`${movieTitle} has been removed from user ${id}'s array`);
+  //} else (
+  //  res.status(404).send('This user is not in the database')
+  //)
+//});
 
-//DELETE: Allow users to deregister
-app.delete('/users/:id/', (req, res) => {
-  const { id } = req.params;
-
-  let user = users.find( user => user.id == id);
-
-  if (user) {
-    users = users.filter( user => user.id != id);
-    res.status(200).send(`user${id} has been removed from the database`);
-  } else (
-    res.status(404).send('This user is not in the database')
-  )
+//DELETE: Allow users to deregister, delete a user by username
+app.delete('/users/:Username', (req, res) => {
+  Users.findOneAndRemove({ Username: req.params.Username })
+    .then((user) => {
+      if (!user) {
+        res.status(400).send(req.params.Username + ' was not found');
+      } else {
+        res.status(200).send(req.params.Username + ' was deleted.');
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
 //READ
 app.get('/', (req, res) => {
-  res.send('Welcome to the movie API!');
+  res.send('Welcome to MyFlix, the movie API!');
 });
 
 //READ: Return a list of ALL movies to the user
 app.get('/movies', (req, res) => {
-  res.status(200).json(movies);
+  Movies.find()
+  .then((movies) => {
+    res.status(200).json(movies);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  });
 });
+  //res.status(200).json(movies);
+//});
 
 //READ: Return data about a single movie by title to the user
 app.get('/movies/:title', (req, res) => {
-  const { title } = req.params;
-  const movie = movies.find( movie => movie.Title === title);
-
-  if (movie) {
+  Movies.findOne({ Title: req.params.title})
+  .then((movie) => {
     res.status(200).json(movie);
-  } else {
-    res.status(404).send('This movie is not in the database')
-  }
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  });
 });
+  //const { title } = req.params;
+  //const movie = movies.find( movie => movie.Title === title);
+
+  //if (movie) {
+  //  res.status(200).json(movie);
+  //} else {
+  //  res.status(404).send('This movie is not in the database')
+  //}
+//});
 
 //READ: Return data about a genre by name/title
 app.get('/movies/genre/:genreName', (req, res) => {
-  const { genreName } = req.params;
-  const genre = movies.find( movie => movie.Genre.Name === genreName).Genre;
-
-  if (genre) {
-    res.status(200).json(genre);
-  } else {
-    res.status(400).send('This genre is not in the database')
-  }
+  Movies.findOne({ "Genre.Name": req.params.Name})
+  .then((movies) => {
+    res.send(movies.Genre);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  });
 });
+  //const { genreName } = req.params;
+  //const genre = movies.find( movie => movie.Genre.Name === genreName).Genre;
+
+  //if (genre) {
+  //  res.status(200).json(genre);
+  //} else {
+  //  res.status(400).send('This genre is not in the database')
+  //}
+//});
 
 //READ: Return data about a director (bio, birth year) by name
 app.get('/movies/director/:directorName', (req, res) => {
-  const { directorName } = req.params;
-  const director = movies.find( movie => movie.Director.Name === directorName).Director;
-
-  if (director) {
-    res.status(200).json(director);
-  } else {
-    res.status(400).send('This director is not in the database')
-  }
+  Movies.findOne({"Director.Name": req.params.Name})
+  .then((movies) => {
+    res.send(movies.Director);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  });
 });
+  //const { directorName } = req.params;
+  //const director = movies.find( movie => movie.Director.Name === directorName).Director;
+
+  //if (director) {
+  //  res.status(200).json(director);
+  //} else {
+  //  res.status(400).send('This director is not in the database')
+  //}
+//});
 
 //Error handling
 app.use(bodyParser.urlencoded({
