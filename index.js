88312a37fe2c0c2d7f581671619281c6bc181100
Angela Lingan Tan <angelalingan@gmail.com>
2022-11-03 19:26:47 -0500

    added passport authenticate in movies endpoint of index.js file of API

--- index.js
+++ index.js
@@ -354,7 +354,7 @@ app.get('/', (req, res) => {
 });
 
 //READ: Return a list of ALL movies to the user
-app.get('/movies', function /*passport.authenticate('jwt', { session: false }),*/(req, res) /*=>*/ {
+app.get('/movies', function passport.authenticate('jwt', { session: false }),* /(req, res) => {
   Movies.find()
     .then((movies) => {
       res.status(200).json(movies);
